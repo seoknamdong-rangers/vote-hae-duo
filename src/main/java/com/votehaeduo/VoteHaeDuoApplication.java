f28afd9bd888650e654f359b518e9b0838c978d0
홍준성 <dundung@woowahan.com>
2022-11-28 21:37:25 +0900

    init project

---
+++ src/main/java/com/votehaeduo/VoteHaeDuoApplication.java
@@ -0,0 +1,13 @@
+package com.votehaeduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteHaeDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteHaeDuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/votehaeduo/VoteHaeDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votehaeduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteHaeDuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
