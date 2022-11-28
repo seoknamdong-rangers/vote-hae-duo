110f111ccee559939e2acc0cffaad0e855f3a81c
홍준성 <dundung@woowahan.com>
2022-11-29 01:14:56 +0900

    vue build, api, port 설정 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  outputDir: path.resolve("__dirname", "../../src/main/resources/static"),
+  devServer: {
+    proxy: {
+      "/": {
+        target: "http://localhost:8080",
+        ws: true,
+        changeOrigin: true
+      }
+    },
+    port: 4000
+  },
+  transpileDependencies: ["vuetify"],
 })
