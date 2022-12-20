e96e8feb220f5a22670615d346eba68ef3f9efc7
sungjun kim <dd8873@naver.com>
2022-12-20 22:00:50 +0900

    feat : VoteController 투표 전체 조회

---
+++ src/main/java/com/votehaeduo/controller/VoteController.java
@@ -0,0 +1,49 @@
+package com.votehaeduo.controller;
+
+import com.votehaeduo.dto.request.VoteInsertRequestDto;
+import com.votehaeduo.dto.request.VoteUpdateRequestDto;
+import com.votehaeduo.dto.response.VoteResponseDto;
+import com.votehaeduo.service.VoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/votes")
+public class VoteController {
+
+    private final VoteService voteService;
+
+    //투표 등록
+    @PostMapping
+    public void save(@RequestBody VoteInsertRequestDto voteInsertRequestDto) {
+        voteService.save(voteInsertRequestDto);
+    }
+
+    //투표 전체 조회
+    @GetMapping
+    public List<VoteResponseDto> findAll() { //페이지를 리턴 하는게 맞음
+        return voteService.findAll();
+    }
+
+    //투표 상세 조회
+    @GetMapping("/{voteId}")
+    public VoteResponseDto findById(@PathVariable("voteId") Long id) {
+        return voteService.findById(id);
+    }
+
+    //투표 수정
+    @PutMapping("/{voteId}")
+    public void update(@PathVariable("voteId") Long id, VoteUpdateRequestDto voteUpdateRequestDto) {
+        voteService.update(id, voteUpdateRequestDto);
+    }
+
+    //투표 삭제
+    @DeleteMapping("/{voteId}")
+    public void delete(@PathVariable("voteId") Long id) {
+        voteService.delete(id);
+    }
+
+}
