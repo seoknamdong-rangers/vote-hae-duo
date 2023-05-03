60c937924fe36721a0e0f54912d9cf6da881a66c
Meringue-KIm <dd8873@naver.com>
2023-05-03 23:23:43 +0900

    test : 투표하기 테스트 코드 추가

--- src/test/java/com/votehaeduo/controller/VoteControllerTest.java
+++ src/test/java/com/votehaeduo/controller/VoteControllerTest.java
@@ -1,13 +1,9 @@
 package com.votehaeduo.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.votehaeduo.dto.request.VoteCreateRequestDto;
-import com.votehaeduo.dto.response.VoteItemCreateResponseDto;
-import com.votehaeduo.dto.response.VoteCreateResponseDto;
-import com.votehaeduo.dto.response.FindVoteItemResponseDto;
-import com.votehaeduo.dto.response.FindVoteResponseDto;
-import com.votehaeduo.dto.response.VoteItemResponseDto;
-import com.votehaeduo.dto.response.VoteResponseDto;
+import com.votehaeduo.dto.request.*;
+import com.votehaeduo.dto.response.*;
 import com.votehaeduo.service.VoteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -55,28 +51,32 @@ class VoteControllerTest {
     @DisplayName("투표 등록 성공")
     void insertVote_success() throws Exception {
         // given
-        VoteCreateResponseDto expectedVoteResponseDto = new VoteCreateResponseDto(1L, "1월 8일 풋살",
+        VoteCreateResponseDto expectedVoteResponseDto = new VoteCreateResponseDto(
+                new VotePayloadResponseDto(1L, "1월 8일 풋살",
+                        LocalDate.of(2023, 1, 20),
+                        LocalDate.of(2023, 1, 30), "성준",
+                        List.of(new VoteItemPayloadResponseDto(1L, "11시 ~ 1시 실외"),
+                                new VoteItemPayloadResponseDto(2L, "12시 ~ 2시 실내"))));
+        given(voteService.create(any())).willReturn(expectedVoteResponseDto);
+
+        VoteCreateRequestDto voteCreateRequestDto = new VoteCreateRequestDto("1월 8일 풋살",
                 LocalDate.of(2023, 1, 20),
                 LocalDate.of(2023, 1, 30), "성준",
-                List.of(new VoteItemCreateResponseDto(1L, "11시 ~ 1시 실외"),
-                        new VoteItemCreateResponseDto(2L, "12시 ~ 2시 실내")));
-        given(voteService.create(any())).willReturn(expectedVoteResponseDto);
-        String voteRequestDtoJsonString = objectMapper.writeValueAsString(expectedVoteResponseDto);
+                List.of(new VoteItemCreateRequestDto("11시 ~ 1시 실외"),
+                        new VoteItemCreateRequestDto("12시 ~ 2시 실내")));
+        String voteRequestDtoJsonString = objectMapper.writeValueAsString(voteCreateRequestDto);
 
         // when & then
         mvc.perform(post("/api/votes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(voteRequestDtoJsonString))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.title").value("1월 8일 풋살"))
-                .andExpect(jsonPath("$.startDate").value("2023-01-20"))
-                .andExpect(jsonPath("$.endDate").value("2023-01-30"))
-                .andExpect(jsonPath("$.createdBy").value("성준"))
-                .andExpect(jsonPath("$.voteItems.[0].id").value(1))
-                .andExpect(jsonPath("$.voteItems.[0].title").value("11시 ~ 1시 실외"))
-                .andExpect(jsonPath("$.voteItems.[1].id").value(2))
-                .andExpect(jsonPath("$.voteItems.[1].title").value("12시 ~ 2시 실내"));
+                .andExpect(jsonPath("$.votePayloadResponseDto.title").value("1월 8일 풋살"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.startDate").value("2023-01-20"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.endDate").value("2023-01-30"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.createdBy").value("성준"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.voteItems.[0].title").value("11시 ~ 1시 실외"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.voteItems.[1].title").value("12시 ~ 2시 실내"));
     }
 
     @Test
@@ -86,6 +86,7 @@ class VoteControllerTest {
         VoteCreateRequestDto voteCreateRequestDto = new VoteCreateRequestDto("1월 8일 풋살",
                 LocalDate.of(2023, 2, 10),
                 LocalDate.of(2023, 2, 20), "성준", List.of());
+
         String json = objectMapper.writeValueAsString(voteCreateRequestDto);
 
         // when & then
@@ -101,7 +102,7 @@ class VoteControllerTest {
     @DisplayName("투표 전체 조회")
     void findAllVote() throws Exception {
         // given
-        List<FindVoteResponseDto> votes = List.of(
+        List<FindVoteResponseDto> expectedVotes = List.of(
                 new FindVoteResponseDto(1L, "12월 15일 풋살 투표",
                         LocalDate.of(2023, 1, 20),
                         LocalDate.of(2023, 1, 30), "성준",
@@ -115,13 +116,10 @@ class VoteControllerTest {
                                 new FindVoteItemResponseDto(2L, "11 ~ 1 야외", Set.of(1L, 2L))),
                         10L)
         );
-        given(voteService.findAll()).willReturn(votes);
-        String voteRequestDtoJsonString = objectMapper.writeValueAsString(votes);
+        given(voteService.findAll()).willReturn(expectedVotes);
 
-        //when & then
-        mvc.perform(get("/api/votes")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(voteRequestDtoJsonString))
+        // when & then
+        mvc.perform(get("/api/votes"))
                 .andExpect(jsonPath("$.[0].id").value(1))
                 .andExpect(jsonPath("$.[0].title").value("12월 15일 풋살 투표"))
                 .andExpect(jsonPath("$.[0].startDate").value("2023-01-20"))
@@ -148,24 +146,50 @@ class VoteControllerTest {
 
     @Test
     @DisplayName("투표 상세 조회")
-    void findByIdVote() {
+    void findByIdVote() throws Exception {
+        //given
+        VoteResponseDto expectedVoteResponseDto = new VoteResponseDto(1L, "1월 10일 풋살",
+                LocalDate.of(2023, 1, 20),
+                LocalDate.of(2023, 1, 30), "성준",
+                List.of(new VoteItemResponseDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L), 2L),
+                        new VoteItemResponseDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L), 2L)),
+                4L);
+        given(voteService.findById(any())).willReturn(expectedVoteResponseDto);
+
+        //when & then
+        mvc.perform(get("/api/votes/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value("1월 10일 풋살"))
+                .andExpect(jsonPath("$.voteItems.[0].id").value(1))
+                .andExpect(jsonPath("$.voteItems.[0].title").value("11시 ~ 1시 실외"))
+                .andExpect(jsonPath("$.voteItems.[1].id").value(2))
+                .andExpect(jsonPath("$.voteItems.[1].title").value("12시 ~ 2시 실내"));
     }
 
     @Test
     @DisplayName("투표 수정")
     void updateVote() throws Exception {
         // given
-        Set<Long> memberIds = Set.of(1L, 2L);
         VoteResponseDto voteResponseDto = new VoteResponseDto(1L, "1월 9일 풋살",
                 LocalDate.of(2023, 1, 20),
                 LocalDate.of(2023, 1, 25),
                 "성준",
-                List.of(new VoteItemResponseDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L)),
-                        new VoteItemResponseDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L))));
+                List.of(new VoteItemResponseDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L), 2L),
+                        new VoteItemResponseDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L), 2L)),
+                4L);
         given(voteService.update(any(), any())).willReturn(voteResponseDto);
-        String voteRequestDtoJsonString = objectMapper.writeValueAsString(voteResponseDto);
 
-        // when, then
+        VoteUpdateRequestDto voteUpdateRequestDto = new VoteUpdateRequestDto("1월 9일 풋살",
+                LocalDate.of(2023, 1, 20),
+                LocalDate.of(2023, 1, 25),
+                "성준",
+                List.of(new VoteItemUpdateRequestDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L)),
+                        new VoteItemUpdateRequestDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L))));
+        String voteRequestDtoJsonString = objectMapper.writeValueAsString(voteUpdateRequestDto);
+
+        // when & then
         mvc.perform(put("/api/votes/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(voteRequestDtoJsonString))
@@ -186,14 +210,52 @@ class VoteControllerTest {
     @Test
     @DisplayName("투표 삭제")
     void deleteVote() throws Exception {
-        //given
+        // given
         given(voteService.delete(any())).willReturn(true);
 
-        //when & then
+        // when & then
         mvc.perform(delete("/api/votes/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().string("true"));
+    }
+
+    @Test
+    @DisplayName("투표 하기")
+    void voting() throws Exception {
+        // given
+        VotingResponseDto expectedVotingResponseDto = new VotingResponseDto(
+                new VotePayloadResponseDto(1L, "1월 8일 풋살",
+                        LocalDate.of(2023, 1, 20),
+                        LocalDate.of(2023, 1, 30), "성준",
+                        List.of(new VoteItemPayloadResponseDto(1L, "11시 ~ 1시 실외"),
+                                new VoteItemPayloadResponseDto(2L, "12시 ~ 2시 실내"))),
+                new VoteMemberResponseDto(Set.of(1L, 2L), 2L),
+                List.of(new VoteMemberResponseDto(Set.of(1L, 2L), 2L),
+                        new VoteMemberResponseDto(Set.of(1L), 1L)));
+        given(voteService.voting(any(), any())).willReturn(expectedVotingResponseDto);
+
+        VotingRequestDto votingRequestDto = new VotingRequestDto(1L, List.of(1L, 2L));
+        String votingRequestDtoJsonString = objectMapper.writeValueAsString(votingRequestDto);
+
+        // when & then
+        mvc.perform(post("/api/votes/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(votingRequestDtoJsonString))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.votePayloadResponseDto.id").value(1))
+                .andExpect(jsonPath("$.votePayloadResponseDto.title").value("1월 8일 풋살"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.startDate").value("2023-01-20"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.endDate").value("2023-01-30"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.createdBy").value("성준"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.voteItems.[0].title").value("11시 ~ 1시 실외"))
+                .andExpect(jsonPath("$.votePayloadResponseDto.voteItems.[1].title").value("12시 ~ 2시 실내"))
+                .andExpect(jsonPath("$.uniqueCount.memberIds.[*]").value(containsInAnyOrder(1, 2)))
+                .andExpect(jsonPath("$.uniqueCount.uniqueCount").value(2))
+                .andExpect(jsonPath("$.uniqueCountByVoteItem.[0].memberIds[*]").value(containsInAnyOrder(1, 2)))
+                .andExpect(jsonPath("$.uniqueCountByVoteItem.[0].uniqueCount").value(2L))
+                .andExpect(jsonPath("$.uniqueCountByVoteItem.[1].memberIds[*]").value(containsInAnyOrder(1)))
+                .andExpect(jsonPath("$.uniqueCountByVoteItem.[1].uniqueCount").value(1L));
     }
 
 }
--- src/test/java/com/votehaeduo/service/VoteServiceTest.java
+++ src/test/java/com/votehaeduo/service/VoteServiceTest.java
@@ -1,14 +1,7 @@
 package com.votehaeduo.service;
 
-import com.votehaeduo.dto.response.FindVoteItemResponseDto;
-import com.votehaeduo.dto.response.FindVoteResponseDto;
-import com.votehaeduo.dto.request.VoteItemUpdateRequestDto;
-import com.votehaeduo.dto.request.VoteUpdateRequestDto;
-import com.votehaeduo.dto.request.VoteItemCreateRequestDto;
-import com.votehaeduo.dto.request.VoteCreateRequestDto;
-import com.votehaeduo.dto.response.VoteCreateResponseDto;
-import com.votehaeduo.dto.response.VoteItemResponseDto;
-import com.votehaeduo.dto.response.VoteResponseDto;
+import com.votehaeduo.dto.request.*;
+import com.votehaeduo.dto.response.*;
 import com.votehaeduo.entity.Vote;
 import com.votehaeduo.entity.VoteItem;
 import com.votehaeduo.exception.vote.VoteNotFoundException;
@@ -89,7 +82,7 @@ class VoteServiceTest {
     @Test
     @DisplayName("투표 전체 조회")
     void findAll() {
-        //given
+        // given
         Long id = new Random().nextLong();
         Vote vote = Vote.builder()
                 .id(id)
@@ -120,7 +113,7 @@ class VoteServiceTest {
                         new FindVoteItemResponseDto(2L, "11 ~ 1 야외", Set.of(1L, 2L))), 2L)
         );
 
-        //when
+        // when
         List<FindVoteResponseDto> voteResponseDto = voteService.findAll();
 
         // then
@@ -128,7 +121,38 @@ class VoteServiceTest {
     }
 
     @Test
-    void findById() {
+    @DisplayName("투표 상세 조회")
+    void findByIdVote() {
+        // given
+        Long id = new Random().nextLong();
+        Vote vote = Vote.builder()
+                .id(id)
+                .title("12월 15일 풋살 투표")
+                .startDate(LocalDate.of(2023, 1, 20))
+                .endDate(LocalDate.of(2023, 1, 30))
+                .createdBy("성준")
+                .build();
+        List<VoteItem> voteItems = List.of(VoteItem.builder()
+                        .id(1L)
+                        .title("12 ~ 2 실내")
+                        .vote(vote)
+                        .memberIds(Set.of(1L, 2L))
+                        .build(),
+                VoteItem.builder()
+                        .id(2L)
+                        .title("11 ~ 1 야외")
+                        .vote(vote)
+                        .memberIds(Set.of(1L, 2L))
+                        .build());
+        vote.addItems(voteItems);
+        VoteResponseDto expectedVoteResponseDto = VoteResponseDto.from(vote);
+        given(voteRepository.findById(any())).willReturn(Optional.of(vote));
+
+        // when
+        VoteResponseDto voteResponseDto = voteService.findById(id);
+
+        // then
+        Assertions.assertThat(voteResponseDto).usingRecursiveComparison().isEqualTo(expectedVoteResponseDto);
     }
 
     @Test
@@ -157,11 +181,12 @@ class VoteServiceTest {
                         .build());
         vote.addItems(voteItems);
         given(voteRepository.findById(any())).willReturn(Optional.of(vote));
-        VoteResponseDto expectedResult = new VoteResponseDto(id, "1월 15일 풋살 투표",
+        VoteResponseDto expectedVoteResponseDto = new VoteResponseDto(id, "1월 15일 풋살 투표",
                 LocalDate.of(2023, 1, 20),
                 LocalDate.of(2023, 1, 25), "성준",
-                List.of(new VoteItemResponseDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L)),
-                        new VoteItemResponseDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L))));
+                List.of(new VoteItemResponseDto(1L, "11시 ~ 1시 실외", Set.of(1L, 2L), 2L),
+                        new VoteItemResponseDto(2L, "12시 ~ 2시 실내", Set.of(3L, 4L), 2L)),
+                4L);
         given(voteRepository.save(any())).willReturn(vote);
 
         // when
@@ -176,7 +201,7 @@ class VoteServiceTest {
                 .build());
 
         // then
-        Assertions.assertThat(voteResponseDto).usingRecursiveComparison().isEqualTo(expectedResult);
+        Assertions.assertThat(voteResponseDto).usingRecursiveComparison().isEqualTo(expectedVoteResponseDto);
     }
 
     @Test
@@ -204,4 +229,53 @@ class VoteServiceTest {
                 .isInstanceOf(VoteNotFoundException.class)
                 .hasMessage("해당 투표를 찾을 수 없습니다.");
     }
+
+    @Test
+    @DisplayName("투표 하기")
+    void voting() {
+        // given
+        Set<Long> firstMemberIds = new HashSet<>();
+        firstMemberIds.add(1L);
+        firstMemberIds.add(2L);
+        Set<Long> secondMemberIds = new HashSet<>();
+        secondMemberIds.add(1L);
+        Long id = new Random().nextLong();
+        Vote vote = Vote.builder()
+                .id(id)
+                .title("1월 8일 풋살")
+                .startDate(LocalDate.of(2023, 1, 20))
+                .endDate(LocalDate.of(2023, 1, 30))
+                .createdBy("성준")
+                .build();
+        List<VoteItem> voteItems = List.of(VoteItem.builder()
+                        .id(1L)
+                        .title("11시 ~ 1시 실외")
+                        .vote(vote)
+                        .memberIds(firstMemberIds)
+                        .build(),
+                VoteItem.builder()
+                        .id(2L)
+                        .title("12시 ~ 2시 실내")
+                        .vote(vote)
+                        .memberIds(secondMemberIds)
+                        .build());
+        vote.addItems(voteItems);
+        given(voteRepository.findById(any())).willReturn(Optional.of(vote));
+        VotingResponseDto expectedVotingResponseDto = new VotingResponseDto(new VotePayloadResponseDto(id, "1월 8일 풋살",
+                LocalDate.of(2023, 1, 20),
+                LocalDate.of(2023, 1, 30), "성준",
+                List.of(new VoteItemPayloadResponseDto(1L, "11시 ~ 1시 실외"),
+                        new VoteItemPayloadResponseDto(2L, "12시 ~ 2시 실내"))),
+                new VoteMemberResponseDto(Set.of(1L, 2L, 7L), 3L),
+                List.of(new VoteMemberResponseDto(Set.of(1L, 2L, 7L), 3L),
+                        new VoteMemberResponseDto(Set.of(1L), 1L)));
+        given(voteRepository.save(any())).willReturn(vote);
+
+        // when
+        VotingResponseDto votingResponseDto = voteService.voting(id, new VotingRequestDto(7L, List.of(1L)));
+
+        // then
+        Assertions.assertThat(votingResponseDto).usingRecursiveComparison().isEqualTo(expectedVotingResponseDto);
+    }
+
 }
